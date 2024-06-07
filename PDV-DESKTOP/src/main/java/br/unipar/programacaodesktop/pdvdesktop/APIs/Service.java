810b9e61264daa1be10936955d743e81135a7c58
Felipe Talini <felipetalini5@gmail.com>
2024-06-06 22:21:49 -0300

    Correcao 2
    Correção do escopo do projeto a adesão de métodos na classe service

--- PDV-DESKTOP/src/main/java/br/unipar/programacaodesktop/pdvdesktop/APIs/Service.java
+++ PDV-DESKTOP/src/main/java/br/unipar/programacaodesktop/pdvdesktop/APIs/Service.java
@@ -4,7 +4,10 @@
  */
 package br.unipar.programacaodesktop.pdvdesktop.APIs;
 
-import br.unipar.programacaodesktop.pdvdesktop.DTO.ClienteDto;
+import br.unipar.programacaodesktop.pdvdesktop.Model.Cliente;
+import br.unipar.programacaodesktop.pdvdesktop.Model.Item_Venda;
+import br.unipar.programacaodesktop.pdvdesktop.Model.Produto;
+import br.unipar.programacaodesktop.pdvdesktop.Model.Venda;
 import br.unipar.programacaodesktop.pdvdesktop.Util;
 import com.google.gson.Gson;
 import java.io.BufferedReader;
@@ -18,10 +21,10 @@ import java.net.URL;
  * @author felip
  */
 public class Service {
-     private static String UrlWebService = "http://127.0.0.1/Api";
+     private static String UrlWebService = "http://localhost/Api";
     private static int sucesso = 200;
     
-    public static ClienteDto buscaCliente(String path) throws Exception{
+    public static Cliente buscaCliente(String path) throws Exception{
         String urlChamada = UrlWebService + path;
         try{
         URL url = new URL(urlChamada);
@@ -32,7 +35,61 @@ public class Service {
             BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
             String json = Util.converteJsontoString(resposta);
             Gson gson = new Gson();
-            ClienteDto dto = gson.fromJson(json, ClienteDto.class);
+            Cliente dto = gson.fromJson(json, Cliente.class);
+            return dto;
+        }
+        catch(IOException | RuntimeException ex){
+            throw new Exception("Erro ao retornar cliente"+ex.getMessage()+"\n"+ex.getStackTrace());
+        }
+    }
+    public static Produto buscaProduto(String path) throws Exception{
+        String urlChamada = UrlWebService + path;
+        try{
+        URL url = new URL(urlChamada);
+        HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+        if(conexao.getResponseCode() != sucesso){
+             throw new RuntimeException("Erro ao conectar: "+conexao.getResponseCode()+" "+conexao.getResponseMessage());
+        }
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = Util.converteJsontoString(resposta);
+            Gson gson = new Gson();
+            Produto dto = gson.fromJson(json, Produto.class);
+            return dto;
+        }
+        catch(IOException | RuntimeException ex){
+            throw new Exception("Erro ao retornar cliente"+ex.getMessage()+"\n"+ex.getStackTrace());
+        }
+    }
+    public static Item_Venda buscaItem_Venda(String path) throws Exception{
+        String urlChamada = UrlWebService + path;
+        try{
+        URL url = new URL(urlChamada);
+        HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+        if(conexao.getResponseCode() != sucesso){
+             throw new RuntimeException("Erro ao conectar: "+conexao.getResponseCode()+" "+conexao.getResponseMessage());
+        }
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = Util.converteJsontoString(resposta);
+            Gson gson = new Gson();
+            Item_Venda dto = gson.fromJson(json, Item_Venda.class);
+            return dto;
+        }
+        catch(IOException | RuntimeException ex){
+            throw new Exception("Erro ao retornar cliente"+ex.getMessage()+"\n"+ex.getStackTrace());
+        }
+    }
+    public static Venda buscaVendas(String path) throws Exception{
+        String urlChamada = UrlWebService + path;
+        try{
+        URL url = new URL(urlChamada);
+        HttpURLConnection conexao = (HttpURLConnection) url.openConnection();
+        if(conexao.getResponseCode() != sucesso){
+             throw new RuntimeException("Erro ao conectar: "+conexao.getResponseCode()+" "+conexao.getResponseMessage());
+        }
+            BufferedReader resposta = new BufferedReader(new InputStreamReader(conexao.getInputStream()));
+            String json = Util.converteJsontoString(resposta);
+            Gson gson = new Gson();
+            Venda dto = gson.fromJson(json, Venda.class);
             return dto;
         }
         catch(IOException | RuntimeException ex){
